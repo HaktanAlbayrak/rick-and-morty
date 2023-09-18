c21c2a65da1bdbc3951b5b87de12d23b47f13c43
Haktan Albayrak <78109456+HaktanAlbayrak@users.noreply.github.com>
2023-09-18 09:59:09 +0300

    search chracter

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,18 +1,45 @@
+import { Clear } from '@mui/icons-material';
+import { IconButton, InputAdornment } from '@mui/material';
 import { FC } from 'react';
 import { StyledTextField } from './TextField.styled';
 
 interface ISearchBarProps {
   labelText: string;
+  value: string;
+  setValue: (value: string) => void;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const SearchBar: FC<ISearchBarProps> = ({ labelText }) => {
+export const SearchBar: FC<ISearchBarProps> = ({
+  labelText,
+  onChange,
+  setValue,
+  value = null,
+}) => {
   return (
     <>
       <StyledTextField
         id='search'
-        label={labelText}
         variant='outlined'
         fullWidth
+        label={labelText}
+        value={value}
+        onChange={onChange}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position='end'>
+              {value !== '' ? (
+                <IconButton
+                  onClick={() => {
+                    setValue('');
+                  }}
+                >
+                  <Clear />
+                </IconButton>
+              ) : null}
+            </InputAdornment>
+          ),
+        }}
       />
     </>
   );
--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -1,11 +1,12 @@
 import { Grid } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
+import { useCallback, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { CharacterCard } from '~/components/cards/character';
+import { SearchBar } from '~/components/search';
 import Section from '~/components/section';
 import { API_URL } from '~/utils/constants';
 import noCharImg from '../../assets/images/no_char_img.jpeg';
-import { SearchBar } from '~/components/search';
 
 export type CharacterStatus = 'Alive' | 'unknown' | 'Dead';
 
@@ -18,14 +19,36 @@ type TItemProps = {
 };
 
 export default function Characters() {
-  const { isLoading, data } = useQuery({
-    queryKey: ['characters'],
-    queryFn: () => fetch(API_URL + 'character').then((res) => res.json()),
+  const [search, setSearch] = useState<string>('');
+  const [errorText, setErrorText] = useState<string | null>(null);
+
+  const fetchCharacterData = useCallback(async () => {
+    let apiUrl = API_URL + 'character';
+
+    if (search !== '') {
+      apiUrl = API_URL + `character?name=${search}`;
+    }
+
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      if (data !== undefined) {
+        return data;
+      }
+    } catch (error) {
+      setErrorText('There is nothing here.');
+    }
+  }, [search]);
+
+  const { data } = useQuery({
+    queryKey: ['characters', search],
+    queryFn: () => fetchCharacterData(),
   });
-
-  if (isLoading) return <h2 className='text-active text-xl'>Loading...</h2>;
-
-  if (data.error) return <h2 className='text-active text-xl'>{data.error}</h2>;
 
   return (
     <Section>
@@ -33,26 +56,37 @@ export default function Characters() {
         <Grid item xs={12}>
           <Grid container justifyContent='center'>
             <Grid item xs={6}>
-              <SearchBar labelText='Search Character' />
+              <SearchBar
+                labelText='Search Character'
+                onChange={(event) => setSearch(event.target.value)}
+                setValue={setSearch}
+                value={search}
+              />
             </Grid>
           </Grid>
         </Grid>
         <Grid item xs={12}>
           <Grid container spacing={4}>
-            {!data.error
-              ? data.results.map((item: TItemProps) => (
-                  <Grid item lg={3} md={4} sm={6} xs={12} key={item.id}>
-                    <Link to='/'>
-                      <CharacterCard
-                        name={item.name}
-                        species={item.species}
-                        status={item.status}
-                        image={item.image !== '' ? item.image : noCharImg}
-                      />
-                    </Link>
-                  </Grid>
-                ))
-              : null}
+            {data ? (
+              data.results.map((item: TItemProps) => (
+                <Grid item lg={3} md={4} sm={6} xs={12} key={item.id}>
+                  <Link to='/'>
+                    <CharacterCard
+                      name={item.name}
+                      species={item.species}
+                      status={item.status}
+                      image={item.image !== '' ? item.image : noCharImg}
+                    />
+                  </Link>
+                </Grid>
+              ))
+            ) : (
+              <Grid item xs={12}>
+                <h2 className='text-active text-xl flex justify-center text-center w-full'>
+                  {errorText}
+                </h2>
+              </Grid>
+            )}
           </Grid>
         </Grid>
       </Grid>
